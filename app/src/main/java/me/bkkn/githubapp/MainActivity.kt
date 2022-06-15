be2cd2794a6ae250b7ad1fd9761a50e68609fab0
bkkn <bakaikin@yandex.ru>
2022-06-15 08:06:13 +0300

    Basic layout added

--- app/src/main/java/me/bkkn/githubapp/MainActivity.kt
+++ app/src/main/java/me/bkkn/githubapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package me.bkkn.githubapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import me.bkkn.githubapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.refeshButton.setOnClickListener {
+            Toast.makeText(this, "refreshed", Toast.LENGTH_LONG).show()
+        }
     }
 }
