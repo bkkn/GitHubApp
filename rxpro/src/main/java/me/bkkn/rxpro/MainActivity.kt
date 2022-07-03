d788d1844bfa7114656ec4f18cfb5d8ff3f2be3d
bkkn <bakaikin@yandex.ru>
2022-07-03 19:03:15 +0200

    layout created

---
+++ rxpro/src/main/java/me/bkkn/rxpro/MainActivity.kt
@@ -0,0 +1,15 @@
+package me.bkkn.rxpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.bkkn.rxpro.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
