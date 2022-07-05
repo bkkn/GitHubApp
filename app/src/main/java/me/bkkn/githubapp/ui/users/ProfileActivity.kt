289def7c4ba3095028e9f4212bbb2d068fde0591
bkkn <bakaikin@yandex.ru>
2022-07-06 00:02:42 +0200

    Serializable - > Parcelable

--- app/src/main/java/me/bkkn/githubapp/ui/users/ProfileActivity.kt
+++ app/src/main/java/me/bkkn/githubapp/ui/users/ProfileActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import coil.load
 import me.bkkn.githubapp.App
-import me.bkkn.githubapp.R
-import me.bkkn.githubapp.databinding.ActivityMainBinding
 import me.bkkn.githubapp.databinding.ActivityProfileBinding
 import me.bkkn.githubapp.domain.entities.UserEntity
 
@@ -17,8 +15,8 @@ class ProfileActivity : AppCompatActivity() {
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val user = intent.extras?.getSerializable(App.EXTRA_USER_KEY) as UserEntity
-        binding.profileActivityLoginTextView.text = user.login
-        binding.profileActivityAvatarImageView.load(user.avatarUrl)
+        val user = intent.extras?.getParcelable<UserEntity>(App.EXTRA_USER_KEY)
+        binding.profileActivityLoginTextView.text = user?.login
+        binding.profileActivityAvatarImageView.load(user?.avatarUrl)
     }
 }
