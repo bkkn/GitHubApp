74786f19bc1a80ac7bef0ce6d5a5b833d87ced57
bkkn <bakaikin@yandex.ru>
2022-07-08 19:00:01 +0300

    Basic DI implemented

--- app/src/main/java/me/bkkn/githubapp/App.kt
+++ app/src/main/java/me/bkkn/githubapp/App.kt
@@ -3,14 +3,28 @@ package me.bkkn.githubapp
 import android.app.Application
 import android.content.Context
 import androidx.fragment.app.Fragment
+import me.bkkn.githubapp.data.retrofit.GithubApi
 import me.bkkn.githubapp.data.retrofit.RetrofitUsersRepoImpl
 import me.bkkn.githubapp.domain.repos.UsersRepo
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class App : Application() {
-    val usersRepo: UsersRepo by lazy { RetrofitUsersRepoImpl() }
+    private val baseUrl = GithubApi.githubUrl
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(GithubApi.githubUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+    }
+    private val api: GithubApi by lazy { retrofit.create(GithubApi::class.java) }
+
+    val usersRepo: UsersRepo by lazy { RetrofitUsersRepoImpl(api) }
 
     companion object Const {
-        const val EXTRA_USER_KEY = "extra_user_key"
+        val EXTRA_USER_KEY = "extra_user_key"
     }
 }
 
