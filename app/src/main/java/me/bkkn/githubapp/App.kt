69e45308cd190db3ab5ce6f426e31613ad813a4b
bkkn <bakaikin@yandex.ru>
2022-07-08 23:47:09 +0200

    some dependencies extracted

--- app/src/main/java/me/bkkn/githubapp/App.kt
+++ app/src/main/java/me/bkkn/githubapp/App.kt
@@ -2,7 +2,10 @@ package me.bkkn.githubapp
 
 import android.app.Application
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
+import me.bkkn.githubapp.data.dummy.FakeUsersRepoImpl
 import me.bkkn.githubapp.data.retrofit.GithubApi
 import me.bkkn.githubapp.data.retrofit.RetrofitUsersRepoImpl
 import me.bkkn.githubapp.domain.repos.UsersRepo
@@ -20,11 +23,13 @@ class App : Application() {
             .build()
     }
     private val api: GithubApi by lazy { retrofit.create(GithubApi::class.java) }
+    private val uiHandelr: Handler by lazy { Handler(Looper.getMainLooper()) }
 
     val usersRepo: UsersRepo by lazy { RetrofitUsersRepoImpl(api) }
+//    val fakeUsersRepo: UsersRepo by lazy { FakeUsersRepoImpl(uiHandelr) }
 
     companion object Const {
-        val EXTRA_USER_KEY = "extra_user_key"
+        const val EXTRA_USER_KEY = "extra_user_key"
     }
 }
 
--- app/src/main/java/me/bkkn/githubapp/data/dummy/FakeUsersRepoImpl.kt
+++ app/src/main/java/me/bkkn/githubapp/data/dummy/FakeUsersRepoImpl.kt
@@ -1,25 +1,27 @@
 package me.bkkn.githubapp.data.dummy
 
 import android.os.Handler
-import android.os.Looper
 import io.reactivex.rxjava3.core.Single
 import me.bkkn.githubapp.domain.entities.UserEntity
 import me.bkkn.githubapp.domain.repos.UsersRepo
 
 private const val DATA_LOADING_FAKE_DELAY = 5_000L
-class FakeUsersRepoImpl : UsersRepo {
+
+class FakeUsersRepoImpl(
+    private val handler: Handler
+) : UsersRepo {
 
     private val data = listOf<UserEntity>(
-        UserEntity("mojombo",1,"https://avatars.githubusercontent.com/u/1?v=4"),
-        UserEntity("defunkt",2,"https://avatars.githubusercontent.com/u/2?v=4"),
-        UserEntity("pjhyett",3,"https://avatars.githubusercontent.com/u/3?v=4"),
+        UserEntity("mojombo", 1, "https://avatars.githubusercontent.com/u/1?v=4"),
+        UserEntity("defunkt", 2, "https://avatars.githubusercontent.com/u/2?v=4"),
+        UserEntity("pjhyett", 3, "https://avatars.githubusercontent.com/u/3?v=4"),
     )
 
     override fun getUsers(
         onSuccess: (List<UserEntity>) -> Unit,
         onError: ((Throwable) -> Unit)?
     ) {
-        Handler(Looper.getMainLooper()).postDelayed({
+        handler.postDelayed({
             onSuccess(data)
         }, DATA_LOADING_FAKE_DELAY)
     }
