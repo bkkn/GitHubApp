002c28cdc454acdd48d2ef18573abf6737369341
bkkn <bakaikin@yandex.ru>
2022-07-12 18:52:10 +0200

    clean up code, liveData wrong names renamed

--- app/src/main/java/me/bkkn/githubapp/ui/users/UsersViewModel.kt
+++ app/src/main/java/me/bkkn/githubapp/ui/users/UsersViewModel.kt
@@ -12,32 +12,32 @@ import me.bkkn.githubapp.domain.repos.UsersRepo
 
 class UsersViewModel(private val usersRepo: UsersRepo) : UsersContract.ViewModel {
 
-    override val usersLiveData: Observable<List<UserEntity>> = BehaviorSubject.create()
-    override val errorLiveData: Observable<Throwable> = BehaviorSubject.create()
-    override val progressLiveData: Observable<Boolean> = BehaviorSubject.create()
+    override val usersObservable: Observable<List<UserEntity>> = BehaviorSubject.create()
+    override val errorObservable: Observable<Throwable> = BehaviorSubject.create()
+    override val progressObservable: Observable<Boolean> = BehaviorSubject.create()
 
     override fun onRefresh() {
         loadData()
     }
 
     override fun onUserDataRequested(id: Int): UserEntity {
-        return usersLiveData.blockingFirst()[id]
+        return usersObservable.blockingFirst()[id]
     }
 
     private fun loadData() {
-        progressLiveData.mutable().onNext(true)
+        progressObservable.mutable().onNext(true)
         usersRepo.getUsers()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeBy(
-            onSuccess = {
-                progressLiveData.mutable().onNext(false)
-                usersLiveData.mutable().onNext(it)
-            },
-            onError = {
-                progressLiveData.mutable().onNext(false)
-                errorLiveData.mutable().onNext(it)
-            }
-        )
+                onSuccess = {
+                    progressObservable.mutable().onNext(false)
+                    usersObservable.mutable().onNext(it)
+                },
+                onError = {
+                    progressObservable.mutable().onNext(false)
+                    errorObservable.mutable().onNext(it)
+                }
+            )
     }
 
     private fun <T> LiveData<T>.mutable(): MutableLiveData<T> {
